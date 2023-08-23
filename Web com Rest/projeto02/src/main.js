2a198d05eea771d2baaed5aa3ee6ae6967f6f263
Matheus Gomes <matt-xp2@live.com>
2023-08-23 20:53:50 -0300

    Adicionando o QR CODE no código

--- Web com Rest/projeto02/src/main.js
+++ Web com Rest/projeto02/src/main.js
@@ -27,3 +27,8 @@ console.log(aluno['nome']);
 //adicionando um novo atributo
 aluno.bolsista = true;
 console.log(aluno);
+
+//QR code gerado via o site nodejs.com
+const qrcode = require('qrcode-terminal');
+
+qrcode.generate('https://web.dio.me/');

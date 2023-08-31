e4a0a6413b5286b274a56e6f1e3ac721a2e9a28c
Matheus Gomes <matt-xp2@live.com>
2023-08-31 19:23:54 -0300

    Adicionando os arquivos com cors

--- Web com Rest/Express/src/main.js
+++ Web com Rest/Express/src/main.js
@@ -1,19 +1,29 @@
-//Importando o módulo express
-const express = require('express');
+// 1. Importar bibliotecas
+const express = require('express'); // importei o módulo express
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-//Criando uma aplicação web
+// 2. Criar o servidor web
 const app = express();
 
-//Rotas para o recurso 'produtos'
+// 3. Configurar o servidor web
+app.use(cors());
+app.use(bodyParser.json()); // aceita dados no formato JSON
+app.use(bodyParser.urlencoded({ extended: true })); // aceita dados no format url encoded
+
+// 4. Configurar as rotas para o recurso 'produtos'
 app.get('/produtos', (req, res) => {
-    res.send('retorna todos os produtos');
+  res.send('retorna todos os produtos cadastrados no sistema');
 });
-
 app.get('/produtos/:id', (req, res) => {
-    res.send('retorna o produto ' + req.params.id);
+  res.send('retorna o produto ' + req.params.id);
+});
+app.post('/produtos', (req, res) => {
+  console.log(req.body);
+  res.send('Falta fazer o código para adicionar um produto');
 });
 
-//Inicia o servidor (ele fica escutando em uma porta)
+// 5. Colocar o servidor no ar
 app.listen(5000, () => {
-    console.log('A aplicação está no ar em http://localhost:5000');
+  console.log('A aplicação está no ar em http://localhost:5000');
 });

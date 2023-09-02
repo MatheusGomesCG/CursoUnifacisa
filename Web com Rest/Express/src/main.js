86b9f528f09eb06c3a030d24fc3551cb1ecd9f48
Matheus Gomes <matt-xp2@live.com>
2023-09-02 09:57:44 -0300

    Adicionando as etapas que falta do codigo

--- Web com Rest/Express/src/main.js
+++ Web com Rest/Express/src/main.js
@@ -12,15 +12,39 @@ app.use(bodyParser.json()); // aceita dados no formato JSON
 app.use(bodyParser.urlencoded({ extended: true })); // aceita dados no format url encoded
 
 // 4. Configurar as rotas para o recurso 'produtos'
+let produtos = []; //Simulando um banco de dados
+
 app.get('/produtos', (req, res) => {
-  res.send('retorna todos os produtos cadastrados no sistema');
+  res.json(produtos);
 });
 app.get('/produtos/:id', (req, res) => {
   res.send('retorna o produto ' + req.params.id);
 });
 app.post('/produtos', (req, res) => {
-  console.log(req.body);
-  res.send('Falta fazer o código para adicionar um produto');
+  produtos.push(req.body);
+  res.send('Produto adicionado com sucesso');
+});
+
+app.put('/produtos/:id', (req, res) => {
+  const id = req.params.id; //Recuperando o valor do ID
+  const index = produtos.findIndex((p) => p.id === id); //Procurando o ID no Array
+  if (index !== -1) { //Só retorna -1 se não encontrar o produto no array
+    produtos[index] = req.body; //Acessando os dados do corpo e adicionando ao produto
+    res.send('Produto atualizado com sucesso');
+  } else {
+    res.status(404).send('Produto não encontrado');
+  }
+});
+
+app.delete('/produtos/:id', (req, res) => {
+  const id = req.params.id; //Recuperando o valor do ID
+  const index = produtos.findIndex((p) => p.id === id); //Procurando o ID no Array
+  if (index !== -1) { //Só retorna -1 se não encontrar o produto no array
+    produtos.splice(index, 1); //Para remover o produto do array
+    res.send('Produto excluído com sucesso');
+  } else {
+    res.status(404).send('Produto não encontrado');
+  }
 });
 
 // 5. Colocar o servidor no ar
